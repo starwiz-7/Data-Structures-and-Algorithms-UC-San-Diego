e1c255b5297242a2a1633ba7421322e4e39ed9c5
Aryan Yadav <54806954+starwiz-7@users.noreply.github.com>
2020-06-04 11:38:09 +0530

    Create reachability.cpp

---
+++ Algorithms on Graphs/Week 1/1_reachability/reachability.cpp
@@ -0,0 +1,65 @@
+// Author: Aryan Yadav
+
+// #include <iostream>
+// #include <vector>
+
+// using std::vector;
+// using std::pair;
+
+// int reach(vector<vector<int> > &adj, int x, int y) {
+//   //write your code here
+//   return 0;
+// }
+
+// int main() {
+//   size_t n, m;
+//   std::cin >> n >> m;
+//   vector<vector<int> > adj(n, vector<int>());
+//   for (size_t i = 0; i < m; i++) {
+//     int x, y;
+//     std::cin >> x >> y;
+//     adj[x - 1].push_back(y - 1);
+//     adj[y - 1].push_back(x - 1);
+//   }
+//   int x, y;
+//   std::cin >> x >> y;
+//   std::cout << reach(adj, x - 1, y - 1);
+// }
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> v[10001];
+bool visited[10001] = {false};
+void dfs(int i)
+{
+  if (visited[i])
+    return;
+  visited[i] = true;
+  for (auto u : v[i])
+  {
+    dfs(u);
+  }
+  return;
+}
+int main()
+{
+  int n, m;
+  cin >> n >> m;
+  for (int i = 0; i < m; i++)
+  {
+    int l, k;
+    cin >> l >> k;
+    v[l].push_back(k);
+    v[k].push_back(l);
+  }
+  int a, b;
+  cin >> a >> b;
+  dfs(a);
+  if (visited[b] == true)
+  {
+    cout << "1";
+  }
+  else
+  {
+    cout << "0";
+  }
+}
